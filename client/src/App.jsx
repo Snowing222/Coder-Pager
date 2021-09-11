50148448b3a222296fcf1614151c20121e655cbd
Snowing222 <snowing222@gmail.com>
2021-09-11 18:59:05 -0400

    ChannelListContainer Sidebar and header

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
+
+import 'stream-chat-react/dist/css/index.css';
+import './App.css';
+
+
+
+const apiKey = "djhgdx6kznpw"
+
+const client = StreamChat.getInstance(apiKey)
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
+        
+     
+      
+    </div>
+  )
+}
+
+export default App
+
