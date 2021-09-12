25310b8d783201eac728e6ddaece9051c1fa4786
Snowing222 <snowing222@gmail.com>
2021-09-12 14:23:05 -0400

    update ChannelList Container

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -23,14 +23,23 @@ const SideBar = ({ logout }) => (
     </div>
 );
 
-
 const CompanyHeader = () => (
     <div className="channel-list__header">
         <p className="channel-list__header__text">Medical Pager</p>
     </div>
 )
 
-const ChannelListContainer = () => {
+const customChannelTeamFilter = (channels) => {
+    return channels.filter((channel) => channel.type === 'team');
+}
+
+const customChannelMessagingFilter = (channels) => {
+    return channels.filter((channel) => channel.type === 'messaging');
+}
+
+const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEditing, setToggleContainer }) => {
+    const { client } = useChatContext();
+
     const logout = () => {
         cookies.remove("token");
         cookies.remove('userId');
@@ -43,36 +52,95 @@ const ChannelListContainer = () => {
         window.location.reload();
     }
 
-
+    const filters = { members: { $in: [client.userID] } };
 
     return (
         <>
-           <SideBar logout = {logout}/>
-           <div className="channel-list__list__wrapper">
+            <SideBar logout={logout} />
+            <div className="channel-list__list__wrapper">
                 <CompanyHeader />
-                <ChannelSearch />
+                <ChannelSearch setToggleContainer={setToggleContainer} />
                 <ChannelList 
-                    filters = {{}} 
-                    channelRenderFilterFn = {()=>{}} 
-                    List={(listProps) => (<TeamChannelList {...listProps} type = "team"/>)} 
+                    filters={filters}
+                    channelRenderFilterFn={customChannelTeamFilter}
+                    List={(listProps) => (
+                        <TeamChannelList 
+                            {...listProps}
+                            type="team"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType} 
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
+                        />
+                    )}
                     Preview={(previewProps) => (
-                        <TeamChannelPreview {...previewProps}
-                        type = "team"/>)}
+                        <TeamChannelPreview 
+                            {...previewProps}
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
+                            type="team"
+                        />
+                    )}
                 />
                 <ChannelList 
-                    filters = {{}} 
-                    channelRenderFilterFn = {()=>{}} 
-                    List={(listProps) => (<TeamChannelList {...listProps} type = "messaging"/>)} 
+                    filters={filters}
+                    channelRenderFilterFn={customChannelMessagingFilter}
+                    List={(listProps) => (
+                        <TeamChannelList 
+                            {...listProps}
+                            type="messaging"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType} 
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
+                        />
+                    )}
                     Preview={(previewProps) => (
-                        <TeamChannelPreview {...previewProps}
-                        type = "messaging"/>)}
+                        <TeamChannelPreview 
+                            {...previewProps}
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
+                            type="messaging"
+                        />
+                    )}
                 />
-
-                
             </div>
-           
         </>
-    )
+    );
 }
 
-export default ChannelListContainer
+const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) => {
+    const [toggleContainer, setToggleContainer] = useState(false);
+
+    return (
+        <>
+            <div className="channel-list__container">
+              <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing} 
+              />
+            </div>
+
+            <div className="channel-list__container-responsive"
+                style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
+            >
+                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
+                </div>
+                <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing}
+                setToggleContainer={setToggleContainer}
+              />
+            </div>
+        </>
+    )
+
+}
+
+export default ChannelListContainer;
--- client/src/components/TeamChannelList.jsx
+++ client/src/components/TeamChannelList.jsx
@@ -28,14 +28,14 @@ const TeamChannelList = ({children, error = false, loading, type}) => {
                 <p className="team-channel-list__header__title">
                     {type === 'team' ? 'Channels' : 'Direct Messages'}
                 </p>
-                {/* <AddChannel 
+                <AddChannel 
                     isCreating={isCreating}
                     setIsCreating={setIsCreating}
                     setCreateType={setCreateType} 
                     setIsEditing={setIsEditing}
                     type={type === 'team' ? 'team' : 'messaging'}
                     setToggleContainer={setToggleContainer}
-                /> */}
+                />
             </div>
             {children}
         </div>
